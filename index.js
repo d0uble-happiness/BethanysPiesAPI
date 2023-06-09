2947c36e9336bb260a2108000253bcbef65e559e
d0uble-happiness <double.happiness.records@gmail.com>
2023-06-09 17:40:28 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+let express = require('express');
+let app = express();
+let pieRepo = require('./repos/pieRepo');
+console.log(pieRepo)
+
+let router = express.Router();
+
+router.get('/', function(req, res, next) {
+    pieRepo.get(function (data) {
+        res.status(200).json({
+            "status": 200
+            ,"statusText": "OK"
+            ,"message": "All pies retrieved."
+            ,"data": data
+        });
+    }, function(err) {
+        next(err);
+    });
+});
+
+app.use('/api/', router);
 
 var server = app.listen(5000, function() {
     console.log('Node server is running on http://localhost:5000..');
